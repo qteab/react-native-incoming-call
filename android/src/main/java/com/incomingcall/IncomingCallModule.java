2daa528780d2805338cff8fab8850073ebcee3b7
huynextlevel <huynextlevel@gmail.com>
2023-11-17 15:24:58 +0700

    feat: resolve native event warning

--- android/src/main/java/com/incomingcall/IncomingCallModule.java
+++ android/src/main/java/com/incomingcall/IncomingCallModule.java
@@ -131,4 +131,14 @@ public class IncomingCallModule extends ReactContextBaseJavaModule {
 
         promise.resolve(null);
     }
+
+    @ReactMethod
+    public void addListener(String eventName) {
+      // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    @ReactMethod
+    public void removeListeners(Integer count) {
+      // Keep: Required for RN built in Event Emitter Calls.
+    }
 }
